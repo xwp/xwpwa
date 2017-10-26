4cf1d422a7b71ad222905990fcd62945ecb70073
Mike Crantea <mike.crantea@xwp.co>
2017-10-26 22:33:30 +0300

    SW - Added precache and loading of workbox from node modules

---
+++ assets/src/js/sw.js
@@ -0,0 +1,86 @@
+importScripts('node_modules/workbox-sw/build/importScripts/workbox-sw.dev.v2.1.0.js');
+
+const workboxSW = new WorkboxSW();
+
+// The precache array gets filled within npm run sw recipe using the configuration inside  workbox-cli-config.js file. This is also called when running npm run theme.
+workboxSW.precache([]);
+
+/*
+
+workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    cacheExpiration: {
+      maxEntries: 20
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+workboxSW.router.registerRoute(/\.(?:png|gif|jpg)$/,
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    cacheExpiration: {
+      maxEntries: 50
+    }
+  })
+);
+
+workboxSW.router.registerRoute('http://weloveiconfonts.com/(.*)',
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'iconfonts',
+    cacheExpiration: {
+      maxEntries: 20,
+      maxAgeSeconds: 7 * 24 * 60 * 60
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+workboxSW.router.registerRoute('/images/icon/*',
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: 'icon-cache',
+    cacheExpiration: {
+      maxEntries: 20
+    }
+  })
+);
+
+var articleHandler = workboxSW.strategies.networkFirst({
+  cacheName: 'articles-cache',
+  cacheExpiration: {
+    maxEntries: 50
+  }
+});
+
+workboxSW.router.registerRoute('/pages/article*.html', args => {
+  return articleHandler.handle(args).then(response => {
+    if (!response) {
+      return caches.match('pages/offline.html');
+    } else if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  });
+});
+
+var postHandler = workboxSW.strategies.cacheFirst({
+  cacheName: 'posts-cache',
+  cacheExpiration: {
+    maxEntries: 50
+  }
+});
+
+workboxSW.router.registerRoute('/pages/post*.html', args => {
+  return postHandler.handle(args).then(response => {
+    if (response.status === 404) {
+      return caches.match('pages/404.html');
+    }
+    return response;
+  })
+  .catch(function() {
+    return caches.match('pages/offline.html');
+  });
+});
+
+*/
