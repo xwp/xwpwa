3871573c0043769eeb524febe1065953896f8935
Mike Crantea <mike.crantea@xwp.co>
2017-10-27 13:32:14 +0300

    Discarded succesful registration console

--- assets/src/js/ServiceWorkerRegistration.js
+++ assets/src/js/ServiceWorkerRegistration.js
@@ -8,10 +8,6 @@ const ServiceWorkerRegistration = {
 	init( serviceWorkerSrc ) {
 		if ( 'serviceWorker' in navigator ) {
 			navigator.serviceWorker.register( serviceWorkerSrc )
-				.then( function( registration ) {
-					console.log( 'Service Worker registration successful with scope: ',
-						registration.scope );
-				})
 				.catch( function( err ) {
 					console.log( 'Service Worker registration failed: ', err );
 				});
