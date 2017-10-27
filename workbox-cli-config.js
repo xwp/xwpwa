1ce3764e3ed7f58a1218e70a5cafed4d16458fe3
Mike Crantea <mike.crantea@xwp.co>
2017-10-27 17:45:57 +0300

    SW Config - cached the manifest.json

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,13 +1,16 @@
+assetsPath = "assets/";
+
 module.exports = {
 	"globDirectory": ".",
 	"globPatterns": [
-		"assets/dist/css/*.css",
-		"assets/dist/js/*.js",
-		"assets/images/blank.png"
+		assetsPath + "dist/css/*.css",
+		assetsPath + "dist/js/*.js",
+		assetsPath + "images/blank.png",
+		assetsPath + "manifest.json"
 	],
 	"globIgnores": [
-		"assets/images/icons/*.{png,xml,ico,svg}"
+		assetsPath + "images/icons/*.{png,xml,ico,svg}"
 	],
-	"swSrc": "assets/src/js/sw.js",
+	"swSrc": assetsPath + "src/js/sw.js",
 	"swDest": "sw.js"
 };
