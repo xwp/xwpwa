2ba677645f28a0768d08824fc555926475f4486e
Mike Crantea <mike.crantea@xwp.co>
2017-10-29 16:09:20 +0200

    SW: Added offline / 404 capabilities with the HTML network first strategy. The regex route needs some work.

--- assets/src/js/sw.js
+++ assets/src/js/sw.js
@@ -5,11 +5,15 @@ importScripts( 'node_modules/workbox-sw/build/importScripts/workbox-sw.' + sw_ve
 const workboxSW = new WorkboxSW();
 
 const webfontsRegex = 'https://(fonts.googleapis.com|fonts.gstatic.com)/(.*)';
-const localImagesRegex = '/(.*).(?:png|gif|jpg|jpeg)$/';
+const localImagesRegex = '/(.*).(png|gif|jpg|jpeg)';
 const cdnImagesRegex = 'https://i(0|1|2).wp.com/(.*)/';
 const avatarsRegex = 'https://(.*).gravatar.com/(.*)/';
-/* TODO: Ignore URLs starting with /wp-admin. */
-const htmlRegex = '/(.*)';
+
+/*
+	TODO: Use http://forbeslindesay.github.io/express-route-tester/ to only \
+  TODO: cache pages outside of the /wp-admin.
+  */
+const htmlRegex = '/assets/html/(.*)';
 
 /*
  1. Cache-Only (Precache)
@@ -22,19 +26,19 @@ workboxSW.precache([]);
  2. Cache First, Network Fallback
  Store in cache all external web fonts, if a regex is defined.
  */
-if ( !! webfontsRegex ) {
+if ( webfontsRegex ) {
 	workboxSW.router.registerRoute( webfontsRegex ,
 		workboxSW.strategies.cacheFirst( {
 			cacheName: 'webfonts',
 			cacheExpiration: {
 				maxEntries: 20
 			},
-			cacheableResponse: {statuses: [0, 200]}
+			cacheableResponse: { statuses: [0, 200] }
 		})
 	);
 }
 
-if ( !! localImagesRegex ) {
+if ( localImagesRegex ) {
 	workboxSW.router.registerRoute( localImagesRegex,
 		workboxSW.strategies.cacheFirst({
 			cacheName: 'images',
@@ -46,15 +50,16 @@ if ( !! localImagesRegex ) {
 	);
 }
 
-if ( !! cdnImagesRegex ) {
-	workboxSW.router.registerRoute( cdnImagesRegex,
-		workboxSW.strategies.cacheFirst({
+if ( cdnImagesRegex ) {
+	workboxSW.router.registerRoute( cdnImagesRegex ,
+		workboxSW.strategies.cacheFirst( {
 			cacheName: 'images',
 			cacheExpiration: {
 				maxEntries: 100,
 				maxAgeSeconds: 14 * 24 * 60 * 60
-			}
-		})
+			},
+			cacheableResponse: { statuses: [0, 200] }
+		} )
 	);
 }
 
@@ -63,14 +68,14 @@ if ( !! cdnImagesRegex ) {
  Store in cache external assets which can change frequently, like avatars,
  which are non-essential to the user experience.
  */
-if ( !! avatarsRegex ) {
+if ( avatarsRegex ) {
 	workboxSW.router.registerRoute( avatarsRegex ,
 		workboxSW.strategies.staleWhileRevalidate( {
 			cacheName: 'avatars',
 			cacheExpiration: {
 				maxEntries: 40
 			},
-			cacheableResponse: {statuses: [0, 200]}
+			cacheableResponse: { statuses: [0, 200] }
 		})
 	);
 }
@@ -79,15 +84,26 @@ if ( !! avatarsRegex ) {
  4. Network First, Cache Fallback
  Store in cache content which always needs to be shown up-to-date.
  */
-if ( !! htmlRegex ) {
-	workboxSW.router.registerRoute( htmlRegex ,
-		workboxSW.strategies.networkFirst( {
-			cacheName: 'html',
-			cacheExpiration: {
-				maxEntries: 50
+if ( htmlRegex ) {
+	var htmlHandler = workboxSW.strategies.networkFirst({
+		cacheName: 'html',
+		cacheExpiration: {
+			maxEntries: 50
+		}
+	});
+
+	workboxSW.router.registerRoute( htmlRegex, args => {
+		return htmlHandler.handle( args ).then( response => {
+			if ( !response ) {
+				return caches.match( 'assets/html/offline.html' );
 			}
-		})
-	);
+			if ( response.status === 404 ) {
+				return caches.match( 'assets/html/404.html' );
+			}
+			return response;
+		});
+	});
+
 }
 
 /*
