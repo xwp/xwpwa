745822692bc98fc59e9e7227074eb9bcef497841
Mike Crantea <mike.crantea@xwp.co>
2017-10-29 18:29:37 +0200

    Compiled SW.

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
 const sw_debug = true;
 const sw_version = sw_debug ? 'dev' : 'prod';
 importScripts( 'node_modules/workbox-sw/build/importScripts/workbox-sw.' + sw_version + '.v2.1.0.js' );
-
 const workboxSW = new WorkboxSW();
 
+/* Cache first */
 const webfontsRegex = 'https://(fonts.googleapis.com|fonts.gstatic.com)/(.*)';
-const localImagesRegex = '/(.*).(?:png|gif|jpg|jpeg)$/';
+const uploadsRegex = '/wp-content/uploads/(.*)';
 const cdnImagesRegex = 'https://i(0|1|2).wp.com/(.*)/';
+/* Stale with revalidate */
 const avatarsRegex = 'https://(.*).gravatar.com/(.*)/';
-/* TODO: Ignore URLs starting with /wp-admin. */
-const htmlRegex = '/(.*)';
+const pluginsRegex = '/wp-content/plugins/(.*)';
+/* Network First */
+const htmlRegex = new RegExp( location.origin + '(.(?!(wp-admin|wp-content)))+' );
 
 /*
  1. Cache-Only (Precache)
@@ -31,7 +33,15 @@ workboxSW.precache([
   },
   {
     "url": "assets/manifest.json",
-    "revision": "c43cc4c4179bd68891fd85e576bff89d"
+    "revision": "af739fec8b93710531f2aef139cbbcbd"
+  },
+  {
+    "url": "assets/html/offline.html",
+    "revision": "bda516cade4b1ddb447a3f885750fdbb"
+  },
+  {
+    "url": "assets/html/404.html",
+    "revision": "7d24a225683f030c329d878470f2eeb7"
   }
 ]);
 
@@ -39,20 +49,20 @@ workboxSW.precache([
  2. Cache First, Network Fallback
  Store in cache all external web fonts, if a regex is defined.
  */
-if ( !! webfontsRegex ) {
+if ( webfontsRegex ) {
 	workboxSW.router.registerRoute( webfontsRegex ,
 		workboxSW.strategies.cacheFirst( {
 			cacheName: 'webfonts',
 			cacheExpiration: {
 				maxEntries: 20
 			},
-			cacheableResponse: {statuses: [0, 200]}
+			cacheableResponse: { statuses: [0, 200] }
 		})
 	);
 }
 
-if ( !! localImagesRegex ) {
-	workboxSW.router.registerRoute( localImagesRegex,
+if ( uploadsRegex ) {
+	workboxSW.router.registerRoute( uploadsRegex,
 		workboxSW.strategies.cacheFirst({
 			cacheName: 'images',
 			cacheExpiration: {
@@ -63,15 +73,16 @@ if ( !! localImagesRegex ) {
 	);
 }
 
-if ( !! cdnImagesRegex ) {
-	workboxSW.router.registerRoute( cdnImagesRegex,
-		workboxSW.strategies.cacheFirst({
+if ( cdnImagesRegex ) {
+	workboxSW.router.registerRoute( cdnImagesRegex ,
+		workboxSW.strategies.cacheFirst( {
 			cacheName: 'images',
 			cacheExpiration: {
 				maxEntries: 100,
 				maxAgeSeconds: 14 * 24 * 60 * 60
-			}
-		})
+			},
+			cacheableResponse: { statuses: [0, 200] }
+		} )
 	);
 }
 
@@ -80,14 +91,25 @@ if ( !! cdnImagesRegex ) {
  Store in cache external assets which can change frequently, like avatars,
  which are non-essential to the user experience.
  */
-if ( !! avatarsRegex ) {
+if ( avatarsRegex ) {
 	workboxSW.router.registerRoute( avatarsRegex ,
 		workboxSW.strategies.staleWhileRevalidate( {
 			cacheName: 'avatars',
 			cacheExpiration: {
 				maxEntries: 40
 			},
-			cacheableResponse: {statuses: [0, 200]}
+			cacheableResponse: { statuses: [0, 200] }
+		})
+	);
+}
+
+if ( pluginsRegex ) {
+	workboxSW.router.registerRoute( pluginsRegex ,
+		workboxSW.strategies.staleWhileRevalidate( {
+			cacheName: 'plugins',
+			cacheExpiration: {
+				maxEntries: 100
+			}
 		})
 	);
 }
@@ -96,52 +118,24 @@ if ( !! avatarsRegex ) {
  4. Network First, Cache Fallback
  Store in cache content which always needs to be shown up-to-date.
  */
-if ( !! htmlRegex ) {
-	workboxSW.router.registerRoute( htmlRegex ,
-		workboxSW.strategies.networkFirst( {
-			cacheName: 'html',
-			cacheExpiration: {
-				maxEntries: 50
+if ( htmlRegex ) {
+	var htmlHandler = workboxSW.strategies.networkFirst({
+		cacheName: 'html',
+		cacheExpiration: {
+			maxEntries: 50
+		}
+	});
+
+	workboxSW.router.registerRoute( htmlRegex, args => {
+		return htmlHandler.handle( args ).then( response => {
+			if ( !response ) {
+				return caches.match( 'assets/html/offline.html' );
 			}
-		})
-	);
+			if ( response.status === 404 ) {
+				return caches.match( 'assets/html/404.html' );
+			}
+			return response;
+		});
+	});
+
 }
-
-/*
-var articleHandler = workboxSW.strategies.networkFirst({
-  cacheName: 'articles-cache',
-  cacheExpiration: {
-    maxEntries: 50
-  }
-});
-
-workboxSW.router.registerRoute('/pages/article*.html', args => {
-  return articleHandler.handle(args).then(response => {
-    if (!response) {
-      return caches.match('pages/offline.html');
-    } else if (response.status === 404) {
-      return caches.match('pages/404.html');
-    }
-    return response;
-  });
-});
-
-var postHandler = workboxSW.strategies.cacheFirst({
-  cacheName: 'posts-cache',
-  cacheExpiration: {
-    maxEntries: 50
-  }
-});
-
-workboxSW.router.registerRoute('/pages/post*.html', args => {
-  return postHandler.handle(args).then(response => {
-    if (response.status === 404) {
-      return caches.match('pages/404.html');
-    }
-    return response;
-  })
-  .catch(function() {
-    return caches.match('pages/offline.html');
-  });
-});
-*/
