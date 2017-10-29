6d72fe532f03f252b1d1b2e9885fd4e63b4726fc
Mike Crantea <mike.crantea@xwp.co>
2017-10-29 16:08:28 +0200

    Added 404 and offline static pages. Precached them.

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,16 +1,18 @@
-assetsPath = "assets/";
+assetsPath = 'assets/';
 
 module.exports = {
-	"globDirectory": ".",
-	"globPatterns": [
-		assetsPath + "dist/css/*.css",
-		assetsPath + "dist/js/*.js",
-		assetsPath + "images/blank.png",
-		assetsPath + "manifest.json"
+	'globDirectory': '.',
+	'globPatterns': [
+		assetsPath + 'dist/css/*.css',
+		assetsPath + 'dist/js/*.js',
+		assetsPath + 'images/blank.png',
+		assetsPath + 'manifest.json',
+		assetsPath + 'html/offline.html',
+		assetsPath + 'html/404.html'
 	],
-	"globIgnores": [
-		assetsPath + "images/icons/*.{png,xml,ico,svg}"
+	'globIgnores': [
+		assetsPath + 'images/icons/*.{png,xml,ico,svg}'
 	],
-	"swSrc": assetsPath + "src/js/sw.js",
-	"swDest": "sw.js"
+	'swSrc': assetsPath + 'src/js/sw.js',
+	'swDest': 'sw.js'
 };
