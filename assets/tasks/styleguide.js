e80cea69d1c31a9d4f48d350d87d2e6356348075
Mike Crantea <mike.crantea@xwp.co>
2018-02-05 11:02:29 +0200

    SC5 - Rewrite the styleguide.js task for easy updating of paths with vars

--- assets/tasks/styleguide.js
+++ assets/tasks/styleguide.js
@@ -1,42 +1,34 @@
 const gulp = require('gulp');
 const styleguide = require('sc5-styleguide/lib/modules/cli/styleguide-cli');
 
+// Paths definitions
+const styleguideTitle = 'Styleguide';
+const readmePath = 'assets/src/css/styleguide.md';
+const kssSourcePath = 'assets/src/css/**/*.scss';
+const styleSourcePath = 'assets/dist/css/*.css';
+
+var outputPath = 'assets/styleguide';
+// Absolute path when hosted
+var appRootPath = '/assets/styleguide';
+
 gulp.task( 'buildStyleguide', function() {
 	let is_development_mode = true;
 	if ( 'production' === process.env.NODE_ENV ) {
 		is_development_mode = false;
 	}
-	styleguide({
-
-		// Styleguide title
-		title: 'Styleguide',
-
-		// Styleguide overview path
-		overviewPath: 'assets/src/css/styleguide.md',
-
-		// KSS source material
-		kssSource: 'assets/src/css/**/*.scss',
-
-		// Stylesheets to include
+	styleguide( {
+		title: styleguideTitle,
+		overviewPath: readmePath,
+		kssSource: kssSourcePath,
 		styleSource: [
-			'assets/dist/css/main.css'
+			styleSourcePath
 		],
-
-		rootPath: '/assets/styleguide',
-
-		appRoot: '/assets/styleguide',
-
-		// Output path
-		output: 'assets/styleguide',
-
-		// Watch for changes
+		rootPath: outputPath,
+		output: outputPath,
+		appRoot: appRootPath,
 		watch: is_development_mode,
-
-		// Serve
 		server: false
-
-	});
-
+	} );
 });
 
 gulp.task('default', [ 'buildStyleguide' ]);
