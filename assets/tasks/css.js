72a8d282c490abdc546b9bb825562d73ebb4423d
Mike Crantea <mike.crantea@xwp.co>
2021-02-25 23:53:12 +0000

    Add watch

--- assets/tasks/css.js
+++ assets/tasks/css.js
@@ -3,14 +3,18 @@ const gulp = require('gulp'),
 			rename = require('gulp-rename');
 
 // Paths definitions
-const scssSourcePath = 'assets/src/css/*.scss';
+const scssSourcePath = 'assets/src/css/';
 const outputPath = 'assets/dist/css';
 
 gulp.task('sass', function(){
-	return gulp.src(scssSourcePath)
+	return gulp.src(scssSourcePath + '*.scss' )
 		.pipe(postcss())
 		.pipe(rename({suffix: '.min', extname: '.css'}))
 		.pipe(gulp.dest(outputPath))
 });
 
-gulp.task( 'default', gulp.series( 'sass' ) );
+gulp.task('watch', function(){
+	gulp.watch(scssSourcePath +  '**/*.scss' , gulp.parallel( 'sass' ));
+})
+
+gulp.task( 'default', gulp.series( 'sass', 'watch' ) );
