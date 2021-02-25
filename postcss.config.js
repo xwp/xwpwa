2d371b42829f561fa7e560d5e4e9f22811959721
Mike Crantea <mike.crantea@xwp.co>
2021-02-25 23:07:35 +0000

    PostCSS compiling - stage 1

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	syntax: 'postcss-scss',
+	plugins: [
+		require( '@csstools/postcss-sass' ),
+		require( 'postcss-color-function' ),
+		require( 'postcss-preset-env' )( {
+			stage: 0,
+			features: {
+				'custom-properties': {
+					preserve: true, // Do not remove :root selector.
+				},
+			},
+			autoprefixer: {
+				grid: true,
+			},
+		} ),
+		require( 'postcss-clean' )
+	],
+};
