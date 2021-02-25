7f2adaeab8743d0edca4114cdd0311bc8b27326a
Mike Crantea <mike.crantea@xwp.co>
2021-02-25 22:12:52 +0000

    Remove unused dependencies

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,7 +1,5 @@
 /* Polyfills */
 /* eslint-disable no-unused-vars */
-import Promise from '../../../node_modules/promise-polyfill/promise';
-import IntersectionObserver from '../../../node_modules/intersection-observer/intersection-observer';
 import Fetch from '../../../node_modules/whatwg-fetch/fetch.js';
 /* eslint-enable no-unused-vars */
 
